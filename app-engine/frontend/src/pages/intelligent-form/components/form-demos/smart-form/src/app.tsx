f09f1ef9539b48d5352b00b0ddc46a8f4eb3d6ec
RonnyChan96 <ronny_chan@126.com>
2025-04-29 14:41:47 +0800

    [app-builder] 智能表单打包文件更新

--- app-engine/frontend/src/pages/intelligent-form/components/form-demos/smart-form/src/app.tsx
+++ app-engine/frontend/src/pages/intelligent-form/components/form-demos/smart-form/src/app.tsx
@@ -31,6 +31,11 @@ export default function App() {
   }
 
   useEffect(() => {
+    window.addEventListener('message', function(event) {
+      if (window.self !== top && event.data) {
+        setReceiveData(event.data);
+      }
+    });
     const ro = new ResizeObserver(entries => {
       entries.forEach(entry => {
         const height = entry.contentRect.height;
@@ -46,198 +51,13 @@ export default function App() {
 
   return (
     <div className='layout' id="custom-smart-form">
-      <DataContext.Provider value={{ data:{data:{"a":"1", "a-options":["1","2"], "a-img":"https://www.gnmxjj.com/uploadfile/202306/85c29e3954fc242.jpg", "a-img-options":["https://www.gnmxjj.com/uploadfile/202306/85c29e3954fc242.jpg","https://www.gnmxjj.com/uploadfile/202306/f47df3e23172196.jpg"], "b": "b","c":"1", "c-options":["1","2"],"d":"1", "d-options":["1","2"], "e":"https://upload.wikimedia.org/wikipedia/commons/5/5f/Liu_Yifei_at_the_2016_BAZAAR_Stars%E2%80%99_Charity_Night.jpg", "f":"qwer"}, schema:{
-            "parameters": [
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "a",
-                "displayName": "radio",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "Radio",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "a-img",
-                "displayName": "radio",
-                "type": "String",
-                "from": "Input",
-                "value": "https://www.gnmxjj.com/uploadfile/202306/85c29e3954fc242.jpg",
-                "renderType": "Radio",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "https://www.gnmxjj.com/uploadfile/202306/85c29e3954fc242.jpg",
-                    "https://www.gnmxjj.com/uploadfile/202306/f47df3e23172196.jpg"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_a459dd59-f59c-4d4d-a198-03be576391c6",
-                "name": "b",
-                "displayName": "input",
-                "type": "String",
-                "from": "Input",
-                "value": "b",
-                "renderType": "Input",
-                "options": {
-                  "id": "e2615231-2420-4d8b-b389-d3f305557f21",
-                  "from": "Reference",
-                  "referenceNode": "",
-                  "referenceId": "",
-                  "referenceKey": "",
-                  "value": [],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "c",
-                "displayName": "checkBox",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "CheckBox",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "c-img",
-                "displayName": "checkBox",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "CheckBox",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "d",
-                "displayName": "select",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "Select",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "d-img",
-                "displayName": "checkBox",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "CheckBox",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "e",
-                "displayName": "image",
-                "type": "String",
-                "from": "Input",
-                "value": "https://upload.wikimedia.org/wikipedia/commons/5/5f/Liu_Yifei_at_the_2016_BAZAAR_Stars%E2%80%99_Charity_Night.jpg",
-                "renderType": "Label",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [],
-                  "type": "Array"
-                }
-              },
-              {
-                "id": "input_69509e2b-fa10-4f85-9e23-f013c50c7905",
-                "name": "f",
-                "displayName": "label",
-                "type": "String",
-                "from": "Input",
-                "value": "1",
-                "renderType": "Label",
-                "options": {
-                  "id": "971306a8-7a24-4d3b-8b9a-2a9dfc77ea92",
-                  "from": "Input",
-                  "referenceNode": null,
-                  "referenceId": null,
-                  "referenceKey": null,
-                  "value": [
-                    "1",
-                    "2"
-                  ],
-                  "type": "Array"
-                }
-              }
-            ]}}, origin: window.location.origin, terminateClick, resumingClick, restartClick}}>
+      <DataContext.Provider value={{ ...receiveData, terminateClick, resumingClick, restartClick}}>
         <SmartForm
-            onSubmit={data => {
-              console.log("data: ", JSON.stringify({"params" : data}));
-              resumingClick({"params" : data});
-            }}
-            onCancel={terminateClick}
+          onSubmit={data => {
+            console.log("data: ", JSON.stringify({"params" : data}));
+            resumingClick({"params" : data});
+          }}
+          onCancel={terminateClick}
         />
       </DataContext.Provider>
     </div>
