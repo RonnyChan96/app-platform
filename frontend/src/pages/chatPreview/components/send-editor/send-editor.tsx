185dddd405a6f784c79005bdd3061270fa9f79ef
17CielYang <17shiqiyang@gmail.com>
2025-10-09 17:45:48 +0800

    [frontend] move upload file button into send-editor

--- frontend/src/pages/chatPreview/components/send-editor/send-editor.tsx
+++ frontend/src/pages/chatPreview/components/send-editor/send-editor.tsx
@@ -80,6 +80,7 @@ const SendEditor = (props) => {
   const [openHistory, setOpenHistory] = useState(false);
   const [positionConfig, setPositionConfig] = useState({});
   const [fileList, setFileList] = useState([]);
+  const [openUploadModal, setOpenUploadModal] = useState(() => null);
   const chatRunning = useAppSelector((state) => state.chatCommonStore.chatRunning);
   const loginStatus = useAppSelector((state) => state.chatCommonStore.loginStatus);
   const showMulti = useAppSelector((state) => state.commonStore.historySwitch);
@@ -285,8 +286,13 @@ const SendEditor = (props) => {
           showMask={showMask}
           setEditorShow={setEditorShow}
           updateUserContext={props.updateUserContext}
+        setExternalUploadOpener={setOpenUploadModal}
         />
         <div className='editor-input' id='drop'>
+        {/* 输入框左侧上传按钮 */}
+        {typeof openUploadModal === 'function' && (
+          <div className='left-upload-icon' onClick={() => openUploadModal()}></div>
+        )}
           <div
             className='chat-promet-editor'
             id='ctrl-promet'
